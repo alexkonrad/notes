c167e1b4e0c3ce190af6a25e55eebbd8ecea90eb
Alex Konrad <alexkonrad08@gmail.com>
2016-06-13 06:16:44 -0500

    add advanced bash scripting

---
+++ advanced-bash-scripting/arithmetic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+let "t2 = ((a = 9, 15 / 3))"
+
+echo $t2, $a
---
+++ advanced-bash-scripting/show-files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Working directory is `pwd`" 
+
+echo "Usage: `basename $0` is the script's filename."
