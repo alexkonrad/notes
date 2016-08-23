77f47edb3ad987da0bf54c0dc0626db26e3bded1
Alex Konrad <alexkonrad08@gmail.com>
2016-08-22 21:42:05 -0700

    add Objective-C

---
+++ objective-c/BNRItem.h
@@ -0,0 +1,21 @@
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
++ (instancetype)randomItem;
+
+- (instancetype)initWithItemName:(NSString *)name
+                valueInDollars:(int)value
+                serialNumber:(NSString *)sNumber;
+
+- (instancetype)initWithItemName:(NSString *)name;
+
+@property (nonatomic, strong) BNRItem  *containedItem;
+@property (nonatomic, weak) BNRItem  *container;
+
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *serialNumber;
+@property (nonatomic) int valueInDollars;
+@property (nonatomic, readonly, strong) NSDate *dateCreated;
+
+@end
